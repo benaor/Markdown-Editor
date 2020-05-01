f64bf5704bf23be7d41e56d91cd281a3874860d4
benaor <benjamingirard25@gmail.com>
2020-05-02 00:43:57 +0200

    display markdown text in div resultat

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import { sampleText } from "./sampleText";
+import marked from "marked";
 
 class App extends Component {
 
   state = {
-    text : sampleText
+    text: sampleText
   }
 
   textChange = (event) => {
     const text = event.target.value
     this.setState({ text })
   }
+
+  renderText = (text) => marked(text, { sanitize: true })
 
   render() {
     return (
@@ -24,16 +27,14 @@ class App extends Component {
             <textarea
               className="form-control"
               rows="30"
-              value={this.state.text} 
-              onChange={this.textChange}/>
+              value={this.state.text}
+              onChange={this.textChange} />
 
           </div>
 
           <div className="col-6">
 
-            <div>
-
-              { sampleText }
+            <div className="resultat" dangerouslySetInnerHTML={{ __html: this.renderText(this.state.text) }}>
 
             </div>
 
